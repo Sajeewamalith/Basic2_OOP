a02bced7bb26ff0d97f2c8a7c08f57784de3487f
sajeewaMalith <sajeewamalith0219@gmail.com>
2022-05-15 15:55:25 +0530

    add TextView using java code only

--- app/src/main/java/com/example/basic2_oop/MainActivity.java
+++ app/src/main/java/com/example/basic2_oop/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.basic2_oop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setText("Wow!");
+
+        setContentView(textView);
     }
 }
